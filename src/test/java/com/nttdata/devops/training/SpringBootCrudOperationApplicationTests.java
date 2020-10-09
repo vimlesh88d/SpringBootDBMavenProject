874a016b5f6adf144cb16aaf9bed30c78e98c956
vimlesh88d <kumarvimlesh.it@gmail.com>
2020-10-09 16:11:13 +0530

    added dependency

--- src/test/java/com/nttdata/devops/training/SpringBootCrudOperationApplicationTests.java
+++ src/test/java/com/nttdata/devops/training/SpringBootCrudOperationApplicationTests.java
@@ -2,8 +2,9 @@ package com.nttdata.devops.training;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
-@SpringBootTest
+@ContextConfiguration
 class SpringBootCrudOperationApplicationTests {
 
 	@Test
